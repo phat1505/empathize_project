9c90b1ac1ebcd0eafafc04b5ba738d6bf0a89976
phat1505 <phat35056723@gmail.com>
2025-11-13 13:26:34 +0700

    Basic Web

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'storm-gust': ['Storm\ Gust', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
